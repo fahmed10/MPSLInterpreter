e198d90b814db82b489a814f29ac6bdca3479a51
fahmed10 <fahmed10@ggc.edu>
2025-10-18 11:15:14 -0400

    Improve FFI support

--- MPSLInterpreter/std_library/FFI.cs
+++ MPSLInterpreter/std_library/FFI.cs
@@ -1,6 +1,7 @@
 using System.Numerics;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Text;
 using static MPSLInterpreter.StdLibrary.LibFFI;
 
 namespace MPSLInterpreter.StdLibrary;
@@ -15,18 +16,36 @@ internal static class FFI
         environment.DefineFunction("get_platform", new(GetPlatform));
         environment.DefineFunction("get_arch", new(GetArch));
         environment.DefineFunction("call", new(CallNative));
-        environment.DefineVariable("void", ffi_type.Void);
-        environment.DefineVariable("pointer", ffi_type.Pointer);
-        environment.DefineVariable("float", ffi_type.Float);
-        environment.DefineVariable("double", ffi_type.Double);
-        environment.DefineVariable("int8", ffi_type.SInt8);
-        environment.DefineVariable("int16", ffi_type.SInt16);
-        environment.DefineVariable("int32", ffi_type.SInt32);
-        environment.DefineVariable("int64", ffi_type.SInt64);
-        environment.DefineVariable("uint8", ffi_type.UInt8);
-        environment.DefineVariable("uint16", ffi_type.UInt16);
-        environment.DefineVariable("uint32", ffi_type.UInt32);
-        environment.DefineVariable("uint64", ffi_type.UInt64);
+
+        MPSLEnvironment pointerGroup = new();
+        pointerGroup.DefineFunction("create", new(CreatePointer));
+        pointerGroup.DefineFunction("create_of", new(CreatePointerTo));
+        pointerGroup.DefineFunction("create_of_array", new(CreatePointerToArray));
+        pointerGroup.DefineFunction("create_of_string", new(CreatePointerToString));
+        pointerGroup.DefineFunction("read_of", new(ReadPointerAs));
+        pointerGroup.DefineFunction("read_of_offset", new(ReadPointerAsOffset));
+        pointerGroup.DefineFunction("read_of_array", new(ReadPointerAsArray));
+        pointerGroup.DefineFunction("read_of_string", new(ReadPointerAsString));
+        pointerGroup.DefineFunction("write_of", new(WritePointerOf));
+        pointerGroup.DefineFunction("write_of_offset", new(WritePointerOfOffset));
+        pointerGroup.DefineFunction("free", new(FreePointer));
+
+        MPSLEnvironment typeGroup = new();
+        typeGroup.DefineVariable("void", ffi_type.Void);
+        typeGroup.DefineVariable("pointer", ffi_type.Pointer);
+        typeGroup.DefineVariable("float", ffi_type.Float);
+        typeGroup.DefineVariable("double", ffi_type.Double);
+        typeGroup.DefineVariable("int8", ffi_type.SInt8);
+        typeGroup.DefineVariable("int16", ffi_type.SInt16);
+        typeGroup.DefineVariable("int32", ffi_type.SInt32);
+        typeGroup.DefineVariable("int64", ffi_type.SInt64);
+        typeGroup.DefineVariable("uint8", ffi_type.UInt8);
+        typeGroup.DefineVariable("uint16", ffi_type.UInt16);
+        typeGroup.DefineVariable("uint32", ffi_type.UInt32);
+        typeGroup.DefineVariable("uint64", ffi_type.UInt64);
+
+        environment.DefineGroup("Pointer", new(pointerGroup));
+        environment.DefineGroup("Type", new(typeGroup));
         return environment;
     }
 
@@ -52,13 +71,105 @@ internal static class FFI
     }
 
     private static string GetArch() => RuntimeInformation.ProcessArchitecture.ToString();
+    private static nint CreatePointer(double size) => Marshal.AllocHGlobal((int)size);
+    private static nint CreatePointerTo(ffi_type type) => Marshal.AllocHGlobal((int)type.size);
+
+    private static unsafe nint CreatePointerToArray(MPSLArray array, ffi_type type)
+    {
+        byte* pointer = (byte*)Marshal.AllocHGlobal(array.Count * (int)type.size);
+
+        for (int i = 0; i < array.Count; i++)
+        {
+            WritePointerOfOffset((nint)pointer, type, array[i], i * (int)type.size);
+        }
+
+        return (nint)pointer;
+    }
+
+    private static unsafe nint CreatePointerToString(string str)
+    {
+        int size = Encoding.UTF8.GetByteCount(str) + 1;
+        byte* pointer = (byte*)Marshal.AllocHGlobal(size);
+        Span<byte> span = new(pointer, size);
+        Encoding.UTF8.TryGetBytes(str.AsSpan(), span, out _);
+        span[size - 1] = 0;
+        return (nint)pointer;
+    }
+
+    private static object? ReadPointerAs(nint pointer, ffi_type type) => ReadPointerAsOffset(pointer, type, 0);
+    private static unsafe object? ReadPointerAsOffset(nint pointer, ffi_type type, double offset)
+    {
+        T Read<T>() where T : unmanaged => MemoryMarshal.Read<T>(new ReadOnlySpan<byte>((byte*)pointer + nint.CreateChecked(offset), (int)type.size));
+
+        if (type == ffi_type.UInt8) return (double)Read<byte>();
+        else if (type == ffi_type.UInt16) return (double)Read<ushort>();
+        else if (type == ffi_type.UInt32) return (double)Read<uint>();
+        else if (type == ffi_type.UInt64) return (double)Read<ulong>();
+        else if (type == ffi_type.SInt8) return (double)Read<sbyte>();
+        else if (type == ffi_type.SInt16) return (double)Read<short>();
+        else if (type == ffi_type.SInt32) return (double)Read<int>();
+        else if (type == ffi_type.SInt64) return (double)Read<long>();
+        else if (type == ffi_type.Float) return (double)Read<float>();
+        else if (type == ffi_type.Double) return Read<double>();
+        else if (type == ffi_type.Pointer) return Read<nint>();
+        else throw new ArgumentException("Invalid pointer type.");
+    }
+
+    private static unsafe string ReadPointerAsString(nint pointer)
+    {
+        int length = 0;
+
+        while (((byte*)pointer)[length] != '\0')
+        {
+            length++;
+        }
+
+        ReadOnlySpan<byte> span = new((byte*)pointer, length);
+        return Encoding.UTF8.GetString(span);
+    }
+
+    private static unsafe MPSLArray ReadPointerAsArray(nint pointer, ffi_type type, double elements)
+    {
+        MPSLArray array = [];
+
+        for (int i = 0; i < elements; i++)
+        {
+            array.Add(ReadPointerAsOffset(pointer, type, (int)type.size * i));
+        }
+
+        return array;
+    }
+
+    private static void WritePointerOf(nint pointer, ffi_type type, object? value) => WritePointerOfOffset(pointer, type, value, 0);
+    private static unsafe void WritePointerOfOffset(nint pointer, ffi_type type, object? value, double offset)
+    {
+        void Write<T, TFrom>() where T : unmanaged, INumber<T> where TFrom : INumber<TFrom>
+        {
+            MemoryMarshal.Write<T>(new((byte*)pointer + nint.CreateChecked(offset), (int)type.size), T.CreateChecked((TFrom)value!));
+        }
+
+        if (type == ffi_type.UInt8) Write<byte, double>();
+        else if (type == ffi_type.UInt16) Write<ushort, double>();
+        else if (type == ffi_type.UInt32) Write<uint, double>();
+        else if (type == ffi_type.UInt64) Write<ulong, double>();
+        else if (type == ffi_type.SInt8) Write<sbyte, double>();
+        else if (type == ffi_type.SInt16) Write<short, double>();
+        else if (type == ffi_type.SInt32) Write<int, double>();
+        else if (type == ffi_type.SInt64) Write<long, double>();
+        else if (type == ffi_type.Float) Write<float, double>();
+        else if (type == ffi_type.Double) Write<double, double>();
+        else if (type == ffi_type.Pointer) Write<nint, nint>();
+        else throw new ArgumentException("Invalid pointer type.");
+    }
+
+    private static void FreePointer(nint pointer) => Marshal.FreeHGlobal(pointer);
 
     private static unsafe object? CallNative(nint handle, string name, MPSLArray args, MPSLArray argTypes, ffi_type returnType)
     {
         void* symbol = (void*)NativeLibrary.GetExport(handle, name);
 
         var argTypesList = stackalloc ffi_type[argTypes.Count];
-        var argTypesArray = new ffi_type*[argTypes.Count];
+        var argTypesArray = stackalloc ffi_type*[argTypes.Count];
 
         for (int i = 0; i < argTypes.Count; i++)
         {
@@ -68,60 +179,30 @@ internal static class FFI
 
         ulong ret;
 
-        fixed (ffi_type** argTypesPtr = argTypesArray)
+        ffi_cif cif = new();
+        ffi_status status = ffi_prep_cif(&cif, ffi_abi.FFI_DEFAULT_ABI, (uint)args.Count, &returnType, argTypesArray);
+
+        void** argValues = stackalloc void*[argTypes.Count];
+
+        for (int i = 0; i < argTypes.Count; i++)
         {
-            ffi_cif cif = new();
-            ffi_status status = ffi_prep_cif(&cif, ffi_abi.FFI_DEFAULT_ABI, (uint)args.Count, &returnType, argTypesPtr);
-
-            void** argValues = stackalloc void*[argTypes.Count];
-
-            for (int i = 0; i < argTypes.Count; i++)
-            {
-                unsafe void AddArgument<T>() where T : unmanaged, INumber<T>
-                {
-                    T* arg = (T*)Marshal.AllocHGlobal((int)argTypesList[i].size);
-                    *arg = T.CreateChecked((double)args[i]!);
-                    argValues[i] = arg;
-                }
-
-                if (argTypesList[i] == ffi_type.UInt8) AddArgument<byte>();
-                else if (argTypesList[i] == ffi_type.UInt16) AddArgument<ushort>();
-                else if (argTypesList[i] == ffi_type.UInt32) AddArgument<uint>();
-                else if (argTypesList[i] == ffi_type.UInt64) AddArgument<ulong>();
-                else if (argTypesList[i] == ffi_type.SInt8) AddArgument<sbyte>();
-                else if (argTypesList[i] == ffi_type.SInt16) AddArgument<short>();
-                else if (argTypesList[i] == ffi_type.SInt32) AddArgument<int>();
-                else if (argTypesList[i] == ffi_type.SInt64) AddArgument<long>();
-                else if (argTypesList[i] == ffi_type.Float) AddArgument<float>();
-                else if (argTypesList[i] == ffi_type.Double) AddArgument<double>();
-                else if (argTypesList[i] == ffi_type.Pointer)
-                {
-                    nint* arg = (nint*)Marshal.AllocHGlobal((int)argTypesList[i].size);
-                    *arg = (nint)args[i]!;
-                    argValues[i] = arg;
-                }
-            }
-
-            ffi_call(&cif, symbol, &ret, argValues);
-
-            for (int i = 0; i < argTypes.Count; i++)
-            {
-                Marshal.FreeHGlobal((nint)argValues[i]);
-            }
+            nint arg = Marshal.AllocHGlobal((int)argTypesList[i].size);
+            WritePointerOf(arg, argTypesList[i], args[i]);
+            argValues[i] = (void*)arg;
         }
 
-        if (returnType == ffi_type.UInt8) return (double)Unsafe.As<ulong, byte>(ref ret);
-        else if (returnType == ffi_type.UInt16) return (double)Unsafe.As<ulong, ushort>(ref ret);
-        else if (returnType == ffi_type.UInt32) return (double)Unsafe.As<ulong, uint>(ref ret);
-        else if (returnType == ffi_type.UInt64) return (double)ret;
-        else if (returnType == ffi_type.SInt8) return (double)Unsafe.As<ulong, sbyte>(ref ret);
-        else if (returnType == ffi_type.SInt16) return (double)Unsafe.As<ulong, short>(ref ret);
-        else if (returnType == ffi_type.SInt32) return (double)Unsafe.As<ulong, int>(ref ret);
-        else if (returnType == ffi_type.SInt64) return (double)Unsafe.As<ulong, long>(ref ret);
-        else if (returnType == ffi_type.Float) return (double)Unsafe.As<ulong, float>(ref ret);
-        else if (returnType == ffi_type.Double) return Unsafe.As<ulong, double>(ref ret);
-        else if (returnType == ffi_type.Pointer) return Unsafe.As<ulong, nint>(ref ret);
-        else if (returnType == ffi_type.Void) return null;
-        else throw new ArgumentException("Invalid return type.");
+        ffi_call(&cif, symbol, &ret, argValues);
+
+        for (int i = 0; i < argTypes.Count; i++)
+        {
+            Marshal.FreeHGlobal((nint)argValues[i]);
+        }
+
+        if (returnType == ffi_type.Void)
+        {
+            return null;
+        }
+
+        return ReadPointerAs((nint)(&ret), returnType);
     }
 }
